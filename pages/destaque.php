cd968f9a32ea6185529b9104809dbdfab7c1c225
jtcosta <jtiagopcosta@gmail.com>
2018-11-16 13:16:36 +0000

    as

---
+++ pages/destaque.php
@@ -0,0 +1,18 @@
+<?php
+  include ('../common/init.php');
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    
+    <title>Page Title</title>
+    <link rel="stylesheet" type="text/css"  href="{$BASE_DIR}css/style.css" />
+</head>
+<body>
+    olá
+    <div id="tres">
+        QUATRO
+    </div>
+</body>
+</html>
