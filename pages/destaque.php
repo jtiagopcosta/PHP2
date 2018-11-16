dbad981da549ea9602bdbdde1271b70a5ecff214
jtcosta <jtiagopcosta@gmail.com>
2018-11-16 17:15:31 +0000

    imagens e página inicial v1

--- pages/destaque.php
+++ pages/destaque.php
@@ -1,22 +1,50 @@
 <?php
  
-  include_once ('../../common/init.php');
+  include_once ('./../common/init.php');
   //include_once ($BASE_DIR . 'database/tweets.php');
 ?>
 <!DOCTYPE html>
 <html>
-<head>
-    <meta charset="utf-8" />
-    
-    <title>Page Title</title>
-    <link rel="stylesheet" type="text/css"  href="<?={$BASE_URL}?>/css/style.css">
-</head>
+    <head>
+        <meta charset="utf-8" />
+        
+        <title>Page Title</title>
+        <link rel="stylesheet" type="text/css"  href="<?=$BASE_URL?>/css/style.css">
+    </head>
 
-<body>
-    olá
-    <div id="tres">
-        QUATRO
-    </div>
+    <body>
+        <div class="top">
+            o
+        </div>
+        <div class="main_container">
+            <div class="container">
+                
+                <div class="header">
+                    Cinéfilos
+                </div>
+                <div class="menu">
+                    <div class="destaques">
+                    Em Destaque
+                    </div>
+                    <div class="filmes">
+                    Filmes
+                    </div>
+                </div>
+                <div class="recent">
+                    <img class="image_recent" src="<?=$BASE_URL?>/img/lock.jpg">
+                    <img class="image_recent" src="<?=$BASE_URL?>/img/john.jpg">
+                    <img class="image_recent" src="<?=$BASE_URL?>/img/fight.jpg">
+                    <img class="image_recent" src="<?=$BASE_URL?>/img/baby.jpg">
+                    <img class="image_recent" src="<?=$BASE_URL?>/img/pulp.jpg">
+                    <img class="image_recent" src="<?=$BASE_URL?>/img/drive.jpg">
+                    <img class="image_recent" src="<?=$BASE_URL?>/img/donnie.jpg">
 
-</body>
+                </div>
+                <div class="foot">
+                    oito
+                </div>
+
+            </div>
+        </div>
+    </body>
 </html>
