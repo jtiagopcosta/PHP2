caf5c73ee827ffc1d208ffeb9a36fe3b3b5f56e1
jtiagopcosta <44501938+jtiagopcosta@users.noreply.github.com>
2018-11-19 12:56:34 +0000

    smartyupdate

--- common/init.php
+++ common/init.php
@@ -1,7 +1,11 @@
 <?php
-  $BASE_DIR = 'trabalhosSiem/trabalhoPHP-2';
+  $BASE_DIR = dirname(__DIR__);
   $BASE_URL = 'https://gnomo.fe.up.pt/~ee11287/trabalhosSiem/trabalhoPHP-2';
 
   $conn = new PDO('pgsql:host=db.fe.up.pt; dbname=siem1818', 'siem1818', 'CMHTnbDy');
   $conn->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+
+  include_once($BASE_DIR . '/lib/smarty/Smarty.class.php');
+  $smarty = new Smarty;
+
 ?>
