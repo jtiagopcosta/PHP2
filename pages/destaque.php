52cf14922db5689a6e5d750ab1830ed9d4333d7c
Caioou <44530299+Caioou@users.noreply.github.com>
2018-11-24 16:16:43 -0200

    e

--- pages/destaque.php
+++ pages/destaque.php
@@ -13,27 +13,7 @@
     </head>
 
     <body>
-        <div class="top">
-            o
-        </div>
-        <div class="main_container">
-            <div class="container">
-                
-                <div class="header">
-                    Cinéfilos
-                </div>
-                <div class="menu">
-                    <nav>
-                        <ul>
-                            <li><a href="#">Em destaque</a></li>
-                            <li><a href="#">Filmes</a></li>
-                            <li><a href="#">Inserir</a></li>
-                        </ul>
-                    </nav>
-                    <div class="search">
-                        Pesquisa
-                    </div>
-                </div>
+    <?php include('header.php') ?>        
                 
                 <div class="recent">
                     <img class="image_recent" src="<?=$BASE_URL?>/img/lock.jpg">
