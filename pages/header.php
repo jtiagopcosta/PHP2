457f857cd96e7d97e3192d76af4e66f7e9368622
Caioou <44530299+Caioou@users.noreply.github.com>
2018-11-24 16:16:09 -0200

    include

---
+++ pages/header.php
@@ -0,0 +1,21 @@
+<div class="top">
+            o
+        </div>
+        <div class="main_container">
+            <div class="container">
+                
+                <div class="header">
+                    Cinéfilos
+                </div>
+                <div class="menu">
+                    <nav>
+                        <ul>
+                            <li><a href="#">Em destaque</a></li>
+                            <li><a href="#">Filmes</a></li>
+                            <li><a href="#">Inserir</a></li>
+                        </ul>
+                    </nav>
+                    <div class="search">
+                        Pesquisa
+                    </div>
+                </div>
