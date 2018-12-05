78fb27be09fd887cd4ad8661b6242507bf50f5e8
Tiago Costa <jtiagopcosta@gmail.com>
2018-12-05 12:51:35 +0000

    header

--- pages/destaque.php
+++ pages/destaque.php
@@ -1,10 +1,6 @@
 <?php
  
-<<<<<<< HEAD
-  include_once('./../common/init.php');
-=======
-  include_once('../common/init.php');
->>>>>>> 5056b7fd178711aa03a7685e175b48366711e25a
+  include_once('../../common/init.php');
                     
   $smarty->display('pages/destaque.tpl'); 
 ?>
